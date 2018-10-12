77f4d6d8d178636297b0d6e84d3bb575ae218f04
Demo <384588466@qq.com>
2018-10-12 14:55:17 +0800

    Initial commit,后台管理项目开发分支

--- config/router.config.js
+++ config/router.config.js
@@ -15,17 +15,6 @@ export default [
 
 
 
-  //test
-  // {
-  //   path: '/test',
-  //   component: '../layouts/UserLayout',
-  //   routes: [
-  //     { path: '/test', redirect: '/user/login' },
-  //     { path: '/test/login', component: './User/Login' },
-  //     { path: '/test/register', component: './User/Register' },
-  //     { path: '/test/register-result', component: './User/RegisterResult' },
-  //   ],
-  // },
 
 
   // 前台系统路由
